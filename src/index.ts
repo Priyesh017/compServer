54afffeb1d72550dc25c38d3f746501abfa06ea5
Mainak908 <ghosh.mainak0401@gmail.com>
2025-03-02 15:47:16 +0530

    some changes by me

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,18 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import router from "./router/mainRouter";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const PORT = 3001;
 
+const generalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 500, // Limit each IP to 100 requests
+  message: "Too many requests, please try again later.",
+});
+
+app.use(generalLimiter);
 app.use(BodyParser.json());
 app.use(cookieParser(process.env.COOKIEP));
 app.use(
