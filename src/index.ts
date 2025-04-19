6cc5e13e1f4779904e63c790f7a99f32b7483135
Priyesh Kumar Saha <priyeshkrs017@gmail.com>
2025-04-19 22:42:26 +0530

    dot

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ app.use(generalLimiter);
 app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ limit: "20kb", extended: true }));
 app.use((_, res, next) => {
-  res.setTimeout(10_000, () => {
+  res.setTimeout(50_000, () => {
     return res.status(504).json({ message: "Request Timeout" });
   });
   next();
